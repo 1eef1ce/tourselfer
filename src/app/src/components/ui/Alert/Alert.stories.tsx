405ecafac70b4e242df6befdebd71a48452db66a
Mariya Kutuzova <mkutuzova@nextype.ru>
2022-02-22 10:58:14 +0500

    24017-дополнены stories  для alert, файл scrollbar.scss не из этого таска, но без него не собирается проект.

--- src/app/src/components/ui/Alert/Alert.stories.tsx
+++ src/app/src/components/ui/Alert/Alert.stories.tsx
@@ -14,4 +14,33 @@ export const Info = Template.bind({});
 Info.args = {
     message: 'Info message',
     title: 'Title alert',
+    icon: {true},
+};
+
+export const Success = Template.bind({});
+Success.args = {
+    type: 'success',
+    message: 'Success message',
+    title: 'Title alert',
+    icon: {true},
+};
+
+export const Error = Template.bind({});
+Error.args = {
+    type: 'error',
+    message: 'Error message',
+    title: 'Title alert',
+    icon: {true},
+};
+
+export const NoTitle = Template.bind({});
+NoTitle.args = {
+    message: 'Info message',
+    icon: {true},
+};
+
+export const NoIcon = Template.bind({});
+NoIcon.args = {
+    title: 'Title alert',
+    message: 'Info message',
 };
