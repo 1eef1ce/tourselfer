b1eaf3952c1bbc716b8587cf4c1c6fa3f9641376
Gleb <gleb@nextype.ru>
2022-03-17 18:28:21 +0500

    25787 Доделал каркас для поисковых подсказок

--- src/app/src/components/common/Searchbar/Searchbar.tsx
+++ src/app/src/components/common/Searchbar/Searchbar.tsx
@@ -1,69 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
 import {SearchIcon} from '@components/icons';
 import {Button} from '@components/ui';
 import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router'
 
-class Searchbar extends React.Component<any, any> {
+
+class SearchbarClass extends React.Component<any, any, any> {
     
     constructor(props) {
         super(props);
         this.state = {
-            inputValue: ''
+            items: []
         };
+
         this.onChange = this.onChange.bind(this);
+        this.fetchData = debounce(this.fetchData, 500);
     }
 
-    onChange() {
-        this.setState({inputValue: (event.target as HTMLInputElement).value});
+    onChange(event) {
+        this.fetchData(event.target.value);
     }
 
+    fetchData(phrase) {
+        const { locale } = this.props.route;
+
+        fetch(process.env.API_HOST + '/api/v1/search/suggestions?language=' + locale + '&q=' + encodeURIComponent(phrase))
+            .then(response => response.json())
+            .then(result => {
+                this.setState({
+                    items: result
+                });
+            });
+    }
+    
     render() {
-        
+        const { t } = this.props.t;
+
         return (
             <form className="form search__form">
                 <div className="search__wrapper">
                     <div className="icon search__icon">
                         <SearchIcon />
                     </div>
-                    <input className="form__field search__field" type="search" placeholder={this.props.t('searchbar.placeholder')}
-                           value={this.state.inputValue} onChange={this.onChange}
-                    />
-                    <Button
-                        className="search__btn"
-                        variant="filled"
-                        type="submit"
-                    >
-                        {this.props.t('button.search')}
-                    </Button>
-                </div>
-            </form>
-        );
-    }
-}
-
-/*const Searchbar = () => {
-    
-    const items = [];
-
-    const { t } = useTranslation("components");
-
-    const onKeyUp = (event) => {
-        this.items[]
-        console.log(event.target.value);
-    };
-
-    let onKeyUp = () => {
-        items.push("ggg");
-    }
-
-    console.log(items);
-    return (
-        <form className="form search__form">
-                <div className="search__wrapper">
-                    <div className="icon search__icon">
-                        <SearchIcon />
-                    </div>
-                    <input className="form__field search__field" type="search" placeholder={t('searchbar.placeholder')} onKeyUp={onKeyUp}/>
+                    <input className="form__field search__field" type="search" placeholder={t('searchbar.placeholder')} onChange={this.onChange} />
                     <Button
                         className="search__btn"
                         variant="filled"
@@ -72,9 +52,32 @@ class Searchbar extends React.Component<any, any> {
                         {t('button.search')}
                     </Button>
                 </div>
-        </form>
-    );
+                {this.state.items && this.state.items.length > 0 &&
+                    <div className="search__suggestions__wrapper">
+                    {this.state.items.map(item => (
+                        <div className="item">
+                            <div className="label">{item.label.general}</div>
+                            {Array.isArray(item.label.subs) && item.label.subs.length > 0 &&
+                                <div className="subs">
+                                    {item.label.subs.join(', ')}
+                                </div>
+                            }
+                        </div>
+                    ))}
+                    </div>
+                }
+            </form>
+            
+        );
+    }
 
-};*/
+}
 
-export default Searchbar;
+const Searchbar = Component => props => {
+    const t = useTranslation('components');
+    const route = useRouter();
+
+    return <Component {...props} t={t} route={route} />;
+};
+
+export default Searchbar(SearchbarClass);
