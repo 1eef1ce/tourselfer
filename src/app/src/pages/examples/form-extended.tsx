d087d0a53edb58d7d4cb227b50c46600159fadac
Yakovenko <eyakovenko@nextype.ru>
2022-04-28 11:21:40 +0500

    26477 works on form components

--- src/app/src/pages/examples/form-extended.tsx
+++ src/app/src/pages/examples/form-extended.tsx
@@ -1,8 +1,9 @@
 import React, {useRef, useState} from 'react';
 import Head from 'next/head';
 import {Layout} from '@components/common';
-import {FileUpload, ImageUpload} from '@components/ui';
+import {FileUpload, ImageUpload, Input} from '@components/ui';
 import NumberFormat from 'react-number-format';
+import { IMaskInput, useIMask } from 'react-imask';
 
 const FormExtendedExample = () => {
     const [file, setFile] = useState('');
@@ -10,6 +11,31 @@ const FormExtendedExample = () => {
     const setImage = (image) => {
         setFile(image)
     };
+
+    function limit(val, max) {
+        if (val.length === 1 && val[0] > max[0]) {
+            val = "0" + val;
+        }
+
+        if (val.length === 2) {
+            if (Number(val) === 0) {
+                val = "01";
+
+                //this can happen when user paste number
+            } else if (val > max) {
+                val = max;
+            }
+        }
+
+        return val;
+    }
+
+    function cardExpiry(val) {
+        const month = limit(val.substring(0, 2), "23");
+        const date = limit(val.substring(2, 4), "59");
+
+        return month + (date.length ? ":" + date : "");
+    }
 
     return (
         <Layout>
@@ -31,11 +57,32 @@ const FormExtendedExample = () => {
                     <div style={{marginTop: "1em"}}>
                         <FileUpload/>
                     </div>
+
                     <div style={{marginTop: "1em"}}>
                         <NumberFormat
-                            format="##:##"
+                            thousandSeparator={true}
+                            prefix="$"
+                            inputMode="numeric"
+                            allowEmptyFormatting={true}
+                        />
+                    </div>
+                    <div style={{marginTop: "1em"}}>
+                        <NumberFormat
+                            format={cardExpiry}
                             mask="_"
                             allowEmptyFormatting={true}
+                            placeholder="__:__"
+                        />
+                    </div>
+
+                    <div style={{marginTop: "1em"}}>
+                        <IMaskInput
+                            mask={Number}
+                            //inputRef={el => this.input = el}
+                            onAccept={
+                                (value, mask) => console.log(value)
+                            }
+                            placeholder='__:__'
                         />
                     </div>
                 </div>
